b0eb1dfe424345d7a42f8ce85973b1aaf6cc7692
Etel <etel.bogen@gmail.com>
2022-03-14 02:16:49 +0300

    Добавила переменные и изменила названия параметров

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно. Диапазон может быть только положительный, включая ноль. А также придумайте, как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.
 
-function getRandom(minNumber, maxNumber) {
-  minNumber = Math.ceil(minNumber);
-  maxNumber = Math.floor(maxNumber);
-  if (minNumber >= 0 && maxNumber > minNumber) {
-    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+const minNumber = 0;
+const maxNumber = 50;
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min >= 0 && max > min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
     return 'Invalid Data Input';
   }
 }
 
-getRandom(0, 50);
+getRandom(minNumber, maxNumber);
 
 //Функцию взяла из документации https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна.
 
-function getCheckCommentLength(commentChecked, maxLength) {
-  if (commentChecked.length < maxLength) {
+const commentChecked = 'Проверка длины строки';
+const maxLength = 140;
+
+function getCheckCommentLength(str, lengthLimit) {
+  if (str.length < lengthLimit) {
     return true;
   } else {
     return false;
   }
 }
 
-getCheckCommentLength('Проверка длины строки', 140);
+getCheckCommentLength(commentChecked, maxLength);
 
 //Функцию взяла из документации https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
