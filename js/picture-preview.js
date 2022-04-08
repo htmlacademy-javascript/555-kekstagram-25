e0e4da7b86806c44ec7f7b3ebf14941eadcc4fce
Etel <etel.bogen@gmail.com>
2022-04-08 13:42:46 +0300

    Исправила фукнцию и вывод количества комментариев

--- js/picture-preview.js
+++ js/picture-preview.js
@@ -3,7 +3,7 @@ import {photoDescription} from './data.js';
 const photoPreviewBlock = document.querySelector('.pictures');//блок для вставки созданных элементов
 const photoPreviewTemplate = document.querySelector('#picture').content;//поиск нужного шаблона для создания элементов]
 
-const usersPhoto = photoDescription; //созданный массив
+const usersPhoto = photoDescription(); //созданный массив как результат работы функции
 window.console.log(usersPhoto);
 
 const photoPreviewFragment = document.createDocumentFragment(); //DOM-объект для вставки сгенерированных DOM-элементов
@@ -12,7 +12,7 @@ usersPhoto.forEach(({url, likes, comments}) => { //превращение пар
   const photoPreviewElement = photoPreviewTemplate.cloneNode(true);//клонирование шаблона для создания элементов
   photoPreviewElement.querySelector('.picture__img').src = url; //адрес изображения url подставляется как атрибут src изображения
   photoPreviewElement.querySelector('.picture__likes').textContent = likes; //количество лайков likes выводится в блок .picture__likes
-  photoPreviewElement.querySelector('.picture__comments').textContent = comments; //количество комментариев comments выводится в блок .picture__comments
+  photoPreviewElement.querySelector('.picture__comments').textContent = comments.length; //количество комментариев comments выводится в блок .picture__comments
   photoPreviewFragment.appendChild(photoPreviewElement); //создание элементов с данными
 });
 
